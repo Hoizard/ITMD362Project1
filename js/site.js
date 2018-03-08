0150f7ce3c9572f55973294fef06f0c88fd50762
Hoizard <31190144+Hoizard@users.noreply.github.com>
2018-03-07 19:28:14 -0600

    Added Javascript code and connection to Index. Fixed some padding to em

--- js/site.js
+++ js/site.js
@@ -3,3 +3,11 @@ jQuery(function($) {
   $('html').removeClass('nojs');
   $('html').addClass('hasjs');
 });
+
+function validateForm(){
+  var x = document.form["myForm"]["name_field"].value;
+  if (x==""){
+    alert("Name must be filled out");
+    return false;
+  }
+}
